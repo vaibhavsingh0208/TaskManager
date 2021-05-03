7302a82492211c26d7425ea5fca8ef645737855c
Vaibhav <vaibhsin@adobe.com>
2021-05-03 18:44:18 +0530

    my changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
-const User = require('../model/user');
-const SECRET_TOKEN = 'TOKEN_VAI_SIN_AUTH';
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const auth = async (req, res, next) => {
-  try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, SECRET_TOKEN);
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-    if (!user) {
-      throw new Error();
-    }
-    req.user = user;
-    req.token = token;
-    next();
-  } catch (error) {
-    res.status(401).send({ error: 'Please authenticate.' });
-    next();
-  }
-};
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-module.exports = auth;
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,81 +1,82 @@
-const express = require('express');
-require('../db/mongoose');
-const User = require('../model/user');
-const auth = require('../middleware/auth');
-
-const router = new express.Router();
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
 
 router.post('/users', async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(400).send({ error });
-  }
-});
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 router.post('/users/login', async (req, res) => {
-  try {
-    const user = await User.findByCredentails(req.body.email, req.body.password);
-    const token = await user.generateAuthToken();
-    res.send({ user, token });
-  } catch (err) {
-    res.status(400).send({ error: 'invalid credentials' });
-  }
-});
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 
 router.post('/users/logout', auth, async (req, res) => {
-  try {
-    console.log(req.token);
-    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
-    console.log(req.user.tokens);
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send({ error });
-  }
-});
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 router.post('/users/logoutAll', auth, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 router.get('/users/me', auth, async (req, res) => {
-  res.send(req.user);
-});
+    res.send(req.user)
+})
 
 router.patch('/users/me', auth, async (req, res) => {
-  const updatableFields = ['name', 'email', 'age', 'password'];
-  const fieldsRecived = Object.keys(req.body);
-  const isValidOperation = fieldsRecived.every(field => updatableFields.includes(field));
-  if (!isValidOperation) {
-    return res.status(400).send('error: Invaild Update!');
-  }
-  fieldsRecived.forEach(field => (req.user[field] = req.body[field]));
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-  try {
-    await req.user.save();
-    res.status(200).send(req.user);
-  } catch (error) {
-    res.status(500).send({ error });
-  }
-});
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 router.delete('/users/me', auth, async (req, res) => {
-  try {
-    await req.user.remove();
-    res.send(req.user);
-  } catch (err) {
-    res.status(500).send({ err });
-  }
-});
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
-module.exports = router;
+module.exports = router
